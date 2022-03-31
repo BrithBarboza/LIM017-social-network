db832e6552dce5b6676a7219b3810fa5d8e5611e
LGomez96 <lindagomez96@gmail.com>
2022-03-31 10:34:46 -0500

    input correo electronico

--- src/components/login.js
+++ src/components/login.js
@@ -3,13 +3,19 @@ import { onNavigate } from '../main.js'; /* la fx para crear el url. */
 
 export const Login = () => {
   const homeDiv = document.createElement('div');
-  homeDiv.textContent = 'Bienvenido al Login';
+  homeDiv.textContent = 'Bienvenido';
+
+  const inputEmail = document.createElement('input');
+  inputEmail.placeholder = 'Ingresa tu correo electronico';
+  inputEmail.type = 'text';
+  inputEmail.id = 'inputEmail';
   const buttonHome = document.createElement('button');
 
   buttonHome.textContent = 'Regresar al Home';
 
   buttonHome.addEventListener('click', () => onNavigate('/'));
 
+  homeDiv.appendChild(inputEmail);
   homeDiv.appendChild(buttonHome);
 
   return homeDiv;
