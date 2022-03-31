43f5b57022c0f0fe0cb3ab554d7f9e9b4ca143e3
BrithBarboza <brith.19.04@gmail.com>
2022-03-31 12:20:55 -0500

    login

--- src/components/login.js
+++ src/components/login.js
@@ -3,20 +3,55 @@ import { onNavigate } from '../main.js'; /* la fx para crear el url. */
 
 export const Login = () => {
   const homeDiv = document.createElement('div');
-  homeDiv.textContent = 'Bienvenido';
+
+  /* const form = document.createElement('div');
+  form.textContent = 'Aquí van los hijos'; */
+
+  const Welcome = document.createElement ('h1');
+  Welcome.textContent = 'Bienvenido'; //?Revisar cómo hacer independiente este label
+  Welcome.id = 'Welcome';
+  Welcome.className = 'Welcome';
+
+  const label1 = document.createElement ('label');
+  label1.textContent = 'Correo electrónico'; //?Revisar cómo hacer independiente este label
+  label1.id = 'label1';
+  label1.className = 'label1';
 
   const inputEmail = document.createElement('input');
   inputEmail.placeholder = 'Ingresa tu correo electronico';
   inputEmail.type = 'text';
   inputEmail.id = 'inputEmail';
+
+  const label2 = document.createElement ('label');
+  label2.textContent = 'Contraseña'; //?Revisar cómo hacer independiente este label
+  label2.id = 'label2';
+  label2.className = 'label2';
+
+  const inputPass = document.createElement('input');
+  inputPass.placeholder = 'Ingresa tu contraseña';
+  inputPass.type = 'text';
+  inputPass.id = 'inputPass';
+
   const buttonHome = document.createElement('button');
 
   buttonHome.textContent = 'Regresar al Home';
 
   buttonHome.addEventListener('click', () => onNavigate('/'));
 
+  /* homeDiv.appendChild(form); */
+  /* homeDiv.appendChild(Welcome); */d
+  homeDiv.insertAdjacentElement("beforeend", Welcome);
+  homeDiv.insertAdjacentElement("beforeend", label1);
+  homeDiv.insertAdjacentElement("beforeend", inputEmail);
+  homeDiv.insertAdjacentElement("beforeend", label2);
+  homeDiv.insertAdjacentElement("beforeend", inputPass);
+  homeDiv.insertAdjacentElement("beforeend", buttonHome);
+
+  /* homeDiv.appendChild(label1);
   homeDiv.appendChild(inputEmail);
-  homeDiv.appendChild(buttonHome);
+  homeDiv.appendChild(label2);
+  homeDiv.appendChild(inputPass);
+  homeDiv.appendChild(buttonHome); */
 
   return homeDiv;
 };
