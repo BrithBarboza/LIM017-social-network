4262ce9a4fe5ca8af0a8ccc6e63413d9de8b277b
LGomez96 <lindagomez96@gmail.com>
2022-04-18 17:57:12 -0500

    estilos del post

--- src/components/feed.js
+++ src/components/feed.js
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-cycle */
 import { onNavigate } from '../main.js';
-import { logOutSocialTravel, addPost } from '../firebaseAuth.js';
+import { logOutSocialTravel, addPost, getPost } from '../firebaseAuth.js';
 
 export const feed = () => {
   const feedDiv = document.createElement('div');
   const templateFeed = `
-  <section id= "feed" class = "contenedor-textos">
+  <section id= "feed" class = "contenedorTextos">
 
   <nav class="Menu" id="Menu">
     <div id = "introduction">
@@ -17,13 +17,11 @@ export const feed = () => {
     </div>
   </nav>
 
-  <h2>Descubre un nuevo lugar para visitar</h2>
+  <h2 class="containerH2"><center>Descubre un nuevo lugar para visitar</center></h2>
 
-  <div id = "post">
-  <div id = "photoProfile">
-  <img src = "./imgns/blank-profile-picture.webp" width = "30px" height = "30px">
-  </div>
-  <input type = "text" class = "cta" placeholder = "¿Quiéres contarnos tu experiencia?">
+  <div id = "btnPostAndPhoto">
+  <img src = "./imgns/blank-profile-picture.webp" id = "photoProfile" width = "40px" height = "40px">
+  <button class = "cta" > ¿Quiéres contarnos tu experiencia? </button>
   </div>
 
   <div class ="modal-container">
@@ -32,7 +30,7 @@ export const feed = () => {
   <div class = "textAndSelect">
   <span class = "textModal">Escribe tu reseña</span>
   <div id = "categories" class ="filter">
-  <select>
+  <select id= "categories">
   <option>Lugares</option>
   <option>Hospedajes</option>
   <option>Restaurantes</option>
@@ -105,6 +103,15 @@ export const feed = () => {
         modalC.style.visibility = 'hidden';
       }, 800);
     }
+  });
+
+  window.addEventListener('DOMContentLoaded', async () => {
+    const querySnapShot = await getPost();
+    // console.log(querySnapShot);
+
+    querySnapShot.forEach(doc => {
+      console.log(doc.data());
+    });
   });
 
   const sendPost = feedDiv.querySelector('#postButton');
